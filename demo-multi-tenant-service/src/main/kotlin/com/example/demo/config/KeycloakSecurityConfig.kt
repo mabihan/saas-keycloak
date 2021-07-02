cc271068e2cafe8d625dd69279ddf9b09cabf15b
CarinaPetravicius <cpetravicius@gmail.com>
2021-07-01 21:54:38 -0300

    Config to no cache authentication.

--- demo-multi-tenant-service/src/main/kotlin/com/example/demo/config/KeycloakSecurityConfig.kt
+++ demo-multi-tenant-service/src/main/kotlin/com/example/demo/config/KeycloakSecurityConfig.kt
@@ -4,16 +4,15 @@ import com.example.demo.filter.RequestFilter
 import org.keycloak.adapters.KeycloakConfigResolver
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
-import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
-import org.springframework.security.core.session.SessionRegistryImpl
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
 
 @Configuration
@@ -23,14 +22,13 @@ class KeycloakSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
         super.configure(http)
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         http.authorizeRequests()
                 .antMatchers("/v1/test").permitAll()
                 .antMatchers("/v1/product").hasAnyRole("branch_office")
                 .anyRequest()
                 .permitAll()
         http.csrf().disable()
-
-        http.addFilterBefore(requestFilter(), KeycloakAuthenticationProcessingFilter::class.java)
     }
 
     @Autowired
@@ -43,7 +41,7 @@ class KeycloakSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
 
     @Bean
     override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
-        return RegisterSessionAuthenticationStrategy(SessionRegistryImpl())
+        return NullAuthenticatedSessionStrategy()
     }
 
     @Bean
